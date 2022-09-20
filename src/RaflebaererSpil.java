29d10a5e3d4740e50bb494dcf823e88d02e246c5
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-09-20 14:06:43 +0200

    Ikke testet method til at slå terning

--- src/RaflebaererSpil.java
+++ src/RaflebaererSpil.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
-
 public class RaflebaererSpil {
+    static int[] slaaTerning() {
+        int terning1 = (int) (Math.random() * 7);
+        int terning2 = (int) (Math.random() * 7);
+        return new int[]{terning1, terning2};
+    }
     public static void main(String[] args) {
-
     }
 
 }
