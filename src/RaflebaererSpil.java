8c6351d99ec7c186a4f167ea03b529893adad0b6
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-09-21 09:32:04 +0200

    method til at slå to terninger, med test som tæller hvor mange dobbelt 6 der er

--- src/RaflebaererSpil.java
+++ src/RaflebaererSpil.java
@@ -1,10 +1,20 @@
 public class RaflebaererSpil {
     static int[] slaaTerning() {
-        int terning1 = (int) (Math.random() * 7);
-        int terning2 = (int) (Math.random() * 7);
+        int terning1 = (int) ((Math.random() * 6)+1);
+        int terning2 = (int) ((Math.random() * 6)+1);
         return new int[]{terning1, terning2};
     }
     public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 100000; i++) {
+            int resultat1 = slaaTerning()[0];
+            int resultat2 = slaaTerning()[1];
+            System.out.println(resultat1 + ", " + resultat2);
+            if (resultat2 == 6 && resultat1 == 6) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
 }
